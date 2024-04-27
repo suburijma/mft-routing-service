1a636e5dc8248968f357d6920f31cce670710237
suburijma <168265021+suburijma@users.noreply.github.com>
2024-04-27 14:52:01 +0530

    Add files via upload

---
+++ mft-routing-service/src/main/java/com/mft/MftRoutingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MftRoutingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MftRoutingServiceApplication.class, args);
+	}
+
+}
---
+++ mft-routing-service/src/test/java/com/mft/MftRoutingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MftRoutingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
